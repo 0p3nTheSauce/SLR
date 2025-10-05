d37599b08605f49449708a7bc21cb698c2f04044
Luke Goodall <ljgoodall2001@gmail.com>
2025-10-05 16:19:12 +0200

    upgraded idle functions for testing

--- code/quefeather.py
+++ code/quefeather.py
@@ -2,7 +2,7 @@
 
 import argparse
 from quewing import daemon, worker, WR_NAME, DN_NAME
-from typing import Literal, TypeAlias
+from typing import Literal, TypeAlias, Optional
 Feather: TypeAlias = Literal["worker", "daemon"]
 FEATHERS = [WR_NAME, DN_NAME]
 
@@ -13,28 +13,38 @@ class queFeather:
     ):
         self.mode = mode
     
-    def run(self,args):
+    def run(self,args, kwargs : Optional[argparse.Namespace] = None):
         if self.mode == "daemon":
-            self.run_daemon(args)
+            self.run_daemon(args, kwargs)
         else:
-            self.run_worker(args)
+            self.run_worker(args, kwargs)
         
-    def run_daemon(self, setting: Literal['watch', 'monitor', 'idle']):
+    def run_daemon(self, setting: Literal['watch', 'monitor', 'idle', 'idle_mon'], args : Optional[argparse.Namespace] = None):
         daem = daemon()
+        print("The daemon sais: ")
         if setting == 'watch':
             daem.start_n_watch()
         elif setting == 'monitor':
             daem.start_n_monitor()
-        else:
+        elif setting == 'idle':
             daem.start_idle()
+        else:
+            print('huh?')
 
-    def run_worker(self, setting: Literal['work', 'idle']):
+    def run_worker(self, setting: Literal['work', 'idle', 'idle_log'], args : Optional[argparse.Namespace] = None):
         wr = worker()
+        print("The worker sais: ")
+        wait = args.wait if args else None
+        cycles = args.cycles if args else None
+        
         if setting == 'work':
             wr.work()
+        elif setting == 'idle':
+            wr.idle("Testing", wait, cycles)
+        elif setting == 'idle_log':
+            wr.idle_log("Testing", wait, cycles)
         else:
-            wr.idle("Testing")
-
+            print('huh?')
 def main():
     parser = argparse.ArgumentParser(prog='quefeather.py')
     subparsers = parser.add_subparsers(dest='mode', help='Operation mode', required=True)
@@ -53,14 +63,30 @@ def main():
 
     worker_parser.add_argument(
         'setting',
-        choices=['work', 'idle'],
+        choices=['work', 'idle', 'idle_log'],
         help='Operation of worker'
+    )
+    
+    worker_parser.add_argument(
+        '-w',
+        '--wait',
+        help='Idle time (seconds)',
+        type=int,
+        default= None
+    )
+    
+    worker_parser.add_argument(
+        '-c',
+        '--cycles',
+        help = 'How many loops',
+        type = int,
+        default = None
     )
 
     args = parser.parse_args()
 
     qf = queFeather(mode=args.mode)
-    qf.run(args)
+    qf.run(args.setting, args)
     
 if __name__ == '__main__':
     main()
