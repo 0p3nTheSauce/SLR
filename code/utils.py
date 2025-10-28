804dccde4e1d4ceef0b6d776ac27bacb0c33b786
Luke Goodall <ljgoodall2001@gmail.com>
2025-10-28 16:52:58 +0200

    refactored the use of determinism to be more consistent and rely soley on configs.

--- code/utils.py
+++ code/utils.py
@@ -9,19 +9,12 @@ from pathlib import Path
 import shutil
 from argparse import ArgumentParser
 import wandb
-import random
 
 from typing import Callable, Optional
 
 #############  Seed ###################
 
-def set_seed(seed=42):
-	torch.manual_seed(seed)
-	torch.cuda.manual_seed_all(seed)
-	np.random.seed(seed)
-	random.seed(seed)
-	torch.backends.cudnn.deterministic = True
-	torch.backends.cudnn.benchmark = False
+
 
 ############## wandb ##################
 class wandb_manager:
