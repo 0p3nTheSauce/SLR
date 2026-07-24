a90b900c0c6dd9dd8c3becc7b849a2f8346ae6de
Luke Goodall <ljgoodall2001@gmail.com>
2026-07-24 15:24:33 +0200

    bit of reshuffling in results

--- src/results/S3D_vs_MViT_32_16/filters.py
+++ src/results/S3D_vs_MViT_32_16/filters.py
@@ -1,14 +1,12 @@
-from src.models import avail_models
-
 acc_cuttoff = 10
-models = avail_models()
+avail_models = ['MViTv2_S', 'MViTv2_S_e', 'MViTv2_S_16x4', 'MViTv2_B_32x3', 'S3D']
 
 additional_modifications = {
     'results': {
         'best_val_acc': lambda x: x > acc_cuttoff
     },
     'admin': {
-        'model': lambda x: x in models
+        'model': lambda x: x in avail_models
     }
 }
 
